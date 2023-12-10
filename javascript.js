93807b1bb7033e2e6c3a98fe56677528da70f40c
Nicholas Vandremo <nicholas.v.vandremo@gmail.com>
2023-12-10 22:08:08 +0100

    Further worked on the web-page. Version 0.0.2

--- javascript.js
+++ javascript.js
@@ -21,3 +21,47 @@ if (cursorIndex) {
 }
 
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  var educationContainer = document.querySelector(".EducationContainer");
+
+  function checkScroll() {
+    var scrollPosition = window.scrollY + window.innerHeight;
+
+    // Adjust the value (500) based on when you want the animation to start
+    if (scrollPosition > 500) {
+      educationContainer.classList.add("appear");
+    }
+  }
+
+  window.addEventListener("scroll", checkScroll);
+});
+
+// Add this script to your JavaScript file
+document.addEventListener("DOMContentLoaded", function () {
+  var educationContainer = document.querySelector(".EducationContainer");
+  var lastScrollPosition = window.scrollY;
+
+  function checkScroll() {
+    var currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scrolling down
+      var scrollPosition = window.scrollY + window.innerHeight;
+
+      // Adjust the value (500) based on when you want the animation to start
+      if (scrollPosition > 500) {
+        educationContainer.classList.add("appear");
+      }
+    } else {
+      // Scrolling up
+      if (currentScrollPosition < 500) {
+        educationContainer.classList.remove("appear");
+      }
+    }
+
+    lastScrollPosition = currentScrollPosition;
+  }
+
+  window.addEventListener("scroll", checkScroll);
+});
